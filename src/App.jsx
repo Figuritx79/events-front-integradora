b0a577ea63273db73c7937cf3cc9bc143b472d36
Enrique Gonzalez <gonherenrique@gmail.com>
2025-01-26 14:55:34 -0600

    Readme listo, tailwind instalado con lucid-icons

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Camera } from 'lucide-react';
+function App() {
+
+  return (
+    < >
+      <h1 className='flex justify-center items-center h-screen text-emerald-500'>Hello integradora</h1>
+      <Camera color="black" size={100} />
+    </>
+  )
+}
+
+export default App
