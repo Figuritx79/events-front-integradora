78f69a426ee8f1cbeddcdc11a63ee5dd55db3f30
Enrique Gonzalez <gonherenrique@gmail.com>
2025-02-24 20:43:02 -0600

    Landing principal casi terminada

---
+++ src/modules/global/components/CardLanding.jsx
@@ -0,0 +1,8 @@
+function CardLanding({ children }) {
+  return (
+    <article className="rounded-lg w-[293px] h-[139px] shadow-[0px_4px_6px_rgba(178,59,196,0.4)]  hover:scale-125 ease-in-out duration-300">
+      {children}
+    </article>
+  );
+}
+export default CardLanding;
